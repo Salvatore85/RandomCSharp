2ab664f70e022308014f92c95c139176a31e2ccd
Salvatore85 <salvatore.m@gmail.com>
2018-08-01 16:07:27 +0200

    More changes to AnagramFinder

--- AnagramFinder/Program.cs
+++ AnagramFinder/Program.cs
@@ -18,20 +18,53 @@ namespace AnagramFinder
                 Console.WriteLine(words);
             }
 
+            string a = "abcd";
+            string b = "bcda"; // bad daa a1b2c3 abc123
+
+            string aa = String.Concat(a.OrderBy(c => c));
+            string bb = String.Concat(b.OrderBy(c => c));
+
+            if (aa == bb)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
             Console.ReadLine();
         }
 
         public static List<string> Anagrams(string word, List<string> words)
         {
-            var testList = new List<string> { "sett", "ttes", "mest", "best", "test" };
             var result = new List<string>();
 
-            foreach (var woordje in testList)
+            //foreach (var woordje in words)
+            //{
+            //    if (woordje == word)
+            //    {
+            //        result.Add(woordje);
+            //    }
+            //}
+            
+            var lettersToCheck = word.ToCharArray();
+
+            foreach (var testWord in words)
             {
-                if (woordje == word)
+                if (testWord.Length == word.Length)
                 {
-                    result.Add(woordje);
+                    var lettersToCompare = testWord.ToCharArray();
+
+                    foreach (var letterToCompare in lettersToCompare)
+                    {
+                    }
                 }
+                else
+                {
+                    return result;
+                }
+                
             }
 
             return result;
