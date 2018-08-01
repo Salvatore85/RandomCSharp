6dc640fac08487f6bc68a2324602afbd738a87cc
Salvatore85 <salvatore.m@gmail.com>
2018-08-01 08:54:08 +0200

    Added Roman numeral converter

---
+++ RomanNumeralsEncoder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace RomanNumeralsEncoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = ConvertNumber(2018);
+
+            Console.WriteLine(number);
+            Console.WriteLine(2018 % 10);
+            Console.ReadLine();
+        }
+
+        public static string ConvertNumber(int n)
+        {
+            string[] thou = { "", "M", "MM", "MMM" };
+            string[] hun  = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] ten  = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] ones = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+            string roman = "";
+
+            roman += thou[(int)(n / 1000) % 10];
+            roman += hun[(int)(n / 100) % 10];
+            roman += ten[(int)(n / 10) % 10];
+            roman += ones[n % 10];
+
+            return roman;
+        }
+    }
+}
