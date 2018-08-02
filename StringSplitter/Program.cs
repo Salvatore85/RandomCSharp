d23650d943d32e82afb924ac2b75a0b4ef259d9c
Salvatore85 <salvatore.m@gmail.com>
2018-08-02 16:02:07 +0200

    Added the StringSplitter project

---
+++ StringSplitter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+//Splits a string into pairs of two characters.
+namespace StringSplitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string TestString = "abcdefg";
+
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+
+        public static string[] Splitter(string str)
+        {
+            //string[] result = str.Split([2]);
+
+            return result;
+        }
+    }
+}
