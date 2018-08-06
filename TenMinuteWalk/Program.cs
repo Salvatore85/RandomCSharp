86fea26801f0fa41fe68b0f8ed2f0d9f6e1f1796
Salvatore85 <salvatore.m@gmail.com>
2018-08-06 11:51:53 +0200

    Added TenMinuteWalk project

---
+++ TenMinuteWalk/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TenMinuteWalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] test = { "n", "n", "n", "s", "n", "s", "n", "s", "n", "s"};
+
+            Console.WriteLine(IsValidWalk(test));
+
+            Console.ReadLine();
+        }
+
+        public static bool IsValidWalk(string[] walk)
+        {
+            if (walk.Length != 10)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
