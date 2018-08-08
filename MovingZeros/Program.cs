fe94fb27f4587361f6d45edaaadc6787ca9f529e
Salvatore85 <salvatore.m@gmail.com>
2018-08-08 10:39:02 +0200

    Added finished project MovingZeros

---
+++ MovingZeros/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovingZeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = {1, 2, 0, 1, 0, 1, 0, 3, 0, 1};
+
+            var newArray = MoveZeroes(test);
+
+            foreach (var number in newArray)
+            {
+                Console.Write(number);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int[] MoveZeroes(int[] arr)
+        {
+            //var result = new List<int>();
+            //int zeroCounter = 0;
+
+            //foreach (var number in arr)
+            //{
+            //    if (number != 0)
+            //    {
+            //        result.Add(number);
+            //    }
+            //    else
+            //    {
+            //        zeroCounter++;
+            //    }
+            //}
+
+            //for (int i = 0; i < zeroCounter; i++)
+            //{
+            //    result.Add(0);
+            //}
+
+            //return result.ToArray();
+
+            return arr.OrderBy(x => x == 0).ToArray();
+        }
+    }
+}
