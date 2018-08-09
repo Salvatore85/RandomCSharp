78b85196c400a770867d1e243bfa51f9cc6d5ce5
Salvatore85 <salvatore.m@gmail.com>
2018-08-09 15:34:53 +0200

    Minor updates

--- MergedStringChecker/Program.cs
+++ MergedStringChecker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace MergedStringChecker
 {
@@ -16,36 +17,49 @@ namespace MergedStringChecker
 
             string test = "Sal vat ore";
 
-            var spaces = test.ToCharArray()
-                .Select((v, i) => new {value = v, index = i})
-                .Where(element => element.value == ' ')
-                .Select((element => element.index))
-                .ToList();
+            StringBuilder sbuilder = new StringBuilder(test);
 
-            foreach (var space in spaces)
-            {
-                Console.WriteLine(space);
-            }
+            char newchar = 'a';
+
+            sbuilder[3] = newchar; 
+
+            Console.WriteLine(sbuilder.ToString());
 
             Console.ReadLine();
         }
 
         public static bool isMerge(string s, string part1, string part2)
         {
+            //Check of sizes of the strings are equal
             if (part1.Length + part2.Length != s.Length * 2 )
             {
                 return false;
             }
 
-            var newPart1 = part1.ToList();
-            var newPart2 = part2.ToList();
-
-            var spaces = s.ToCharArray()
+            //Get the indexes of the spaces in part1
+            var spaceIndexes = part1.ToCharArray()
                 .Select((v, i) => new { value = v, index = i })
                 .Where(element => element.value == ' ')
                 .Select((element => element.index))
                 .ToList();
 
+            //Remove spaces from part2
+            string trimmedPart2 = part2.Replace(" ", "");
+
+            StringBuilder builder = new StringBuilder(s);
+
+            int counter = spaceIndexes.Count;
+
+            //foreach (var VARIABLE in COLLECTION)
+            //{
+            //    builder[index#] = trimmedpart2[index#]
+            //    counter--; 
+            //}
+
+            if (builder.ToString() == s)
+            {
+                return true;
+            }
 
             return false;
         }
