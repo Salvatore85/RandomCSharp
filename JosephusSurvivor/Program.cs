228d25983edb182d3226739215b6370c7d5db857
Salvatore85 <salvatore.m@gmail.com>
2018-08-13 14:18:12 +0200

    Added the JosephusSurvivor project

---
+++ JosephusSurvivor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JosephusSurvivor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
